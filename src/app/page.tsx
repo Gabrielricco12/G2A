af91fc8bd0b96e6162f2381121c83af1d37c6317
Gabrielricco12 <gabrielricco2023@gmail.com>
2026-02-07 16:01:50 -0300

    Change redirect from dashboard to login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import { redirect } from "next/navigation";
 
 export default function Home() {
-  // Assim que o usuário entrar em "g2-2jvcpng0p...", ele é jogado para o dashboard
-  redirect("/dashboard");
+  // Redireciona imediatamente para a tela de login
+  redirect("/login");
 }
